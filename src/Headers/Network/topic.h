7a0ef56677f90877422cf94e90ab08fcdd1bbdf0
kou1153 <71371833+kou1153@users.noreply.github.com>
2022-12-08 14:33:43 +0700

    Refactored code

--- src/Headers/Network/topic.h
+++ src/Headers/Network/topic.h
@@ -1,8 +1,23 @@
 #pragma once
 
+String setupString(String options);
+
+const String deviceName = "espInCameraPacks";
+const String roomName = "room1";
+const String packetACK = "ACK";
+const String serverName = "server";
 // Setup topic
 // From device
-const char *topicCameraPacksDevice = "espInCameraPacks/room1";
-const char *topicCameraPacksACK = "espInCameraPacks/room1/ACK";
+const char *topicCameraPacksDevice = setupString("device").c_str();
+const char *topicCameraPacksACK = setupString("ack").c_str();
 // To device
-const char *topicCameraPacksServer = "espInCameraPacks/room1/server";
+const char *topicCameraPacksServer = setupString("server").c_str();
+
+String setupString(String options)
+{
+    String temp = deviceName + "/" + roomName;
+    if (options == "device") return temp;
+    else if (options == "ack") return temp += "/" + packetACK;
+    else if (options == "server") return temp += "/" + serverName;
+    else return options;
+}
