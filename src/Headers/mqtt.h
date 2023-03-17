6ded6b371cd6a748f21a83c0c8c3209ab1fcf2fa
Quang Minh <71371833+kou1153@users.noreply.github.com>
2023-03-17 13:41:55 +0700

    update api & mqtt creds

--- src/Headers/mqtt.h
+++ src/Headers/mqtt.h
@@ -8,9 +8,9 @@
 #include "api.h"
 
 // Setup MQTT
-inline const char *mqttServer = "192.168.68.116";
-inline const char *mqttUsername = "pi";
-inline const char *mqttPassword = "Kou-chan1153";
+inline const char *mqttServer = "rndaedss.ddns.net";
+inline const char *mqttUsername = "aws";
+inline const char *mqttPassword = "Rnd_AEDSS2023";
 inline int port = 1883;
 
 inline const char *mqttClientID = String(ESP.getChipId()).c_str();
